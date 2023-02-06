f5c2d5ca8260a910790683eb91400f727f1b0953
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-06 14:48:25 +0300

    clean format

--- tryCatchError/TryCstchSyntax.js
+++ tryCatchError/TryCstchSyntax.js
@@ -1,14 +1,14 @@
 try {
-   hello; //an error to see if it will be catched
-    alert('try'); //to alert us if anything happen
- }
- catch(error){
-   console.log(error);
-    alert('catch'); //to alert us if it catched an error
- }
- finally {
-   alert('finally'); //to tell us that it ended, its optional
- }
+  hello; //an error to see if it will be catched
+  alert('try'); //to alert us if anything happen
+}
+catch (error) {
+  console.log(error);
+  alert('catch'); //to alert us if it catched an error
+}
+finally {
+  alert('finally'); //to tell us that it ended, its optional
+}
 
 
 //  try and catch is a run time error only
--- tryCatchError/index.js
+++ tryCatchError/index.js
@@ -1,15 +1,13 @@
-function area(width, hight){
-    if(isNaN(width) || isNaN(hight)){
-        throw new Error ("only numbers are allowed");
-    }
-    return width * hight;
-}
-
-
-try {
-   area(5,'hi') // trying to catch an error
-}
-  catch(error){
-    alert('error in area');
-    console.log(error);
+function area(width, hight) {
+  if (isNaN(width) || isNaN(hight)) {
+    throw new Error("only numbers are allowed");
   }
+  return width * hight;
+}
+try {
+  area(5, 'hi') // trying to catch an error
+}
+catch (error) {
+  alert('error in area');
+  console.log(error);
+}
