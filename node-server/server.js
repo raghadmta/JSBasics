aec82cacb720ca71f335e9ba4bb15039314f5edf
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-07 17:22:09 +0300

    added get and fetch

--- node-server/server.js
+++ node-server/server.js
@@ -6,5 +6,12 @@ app.get('/', (req, res) => { //how to handle the requset from the user
     res.sendFile('index.html', {root: __dirname});
 });
 
+app.get('/user', (req, res) => res.json([ // the user will requset from the path with the 'user' suffix
+    {id:1, name:"raghad", email:'example1@example.com'},
+    {id:2, name:"abeer", email:'example2@example.com'},
+    {id:3, name:"manahel", email:'example3@example.com'},
+
+]));
+
 //server listen to port number:
 app.listen(3000, () => console.log('server is listening in port 3000!'));
