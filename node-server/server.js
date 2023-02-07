ada679dd1ff2edef72c61dc234e3d840ad8f9152
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-07 18:35:45 +0300

    send data to the server

--- node-server/server.js
+++ node-server/server.js
@@ -1,10 +1,15 @@
 const express = require('express'); //import express
+const bodyParser =require('body-parser'); // after installing body parser -> npm i body-parser 
+const app = express(); // call function to create a server 
 
-const app = express(); // call function to create aserver 
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => { //how to handle the requset from the user
     res.sendFile('index.html', {root: __dirname});
 });
+
+
 
 app.get('/user', (req, res) => res.json([ // the user will requset from the path with the 'user' suffix
     {id:1, name:"raghad", email:'example1@example.com'},
@@ -12,6 +17,12 @@ app.get('/user', (req, res) => res.json([ // the user will requset from the path
     {id:3, name:"manahel", email:'example3@example.com'},
 
 ]));
+
+
+app.post('/user', (req, res) =>{ //to add the user, post is to sent the data to the server
+    console.log(req.body);
+    res.json({message: "user added successfully!"});
+})
 
 //server listen to port number:
 app.listen(3000, () => console.log('server is listening in port 3000!'));
