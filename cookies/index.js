50b5b2657ec910169c677e3a924bce3b497b870b
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-08 16:50:52 +0300

    add cookies

---
+++ cookies/index.js
@@ -0,0 +1,5 @@
+//Cookies
+document.cookie=`email=example@example.com;expires=${ new Date(2023, 2, 8).toUTCString()}`; // to add an expiration date to the cookies
+//Cookies
+document.cookie=`name=raghad;expires=${ new Date(2023, 2, 8).toUTCString()}`; 
+// run document.cookie in the conosle to print the cookies
