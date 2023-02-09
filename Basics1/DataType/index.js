5dfe73cecc5a552011793f5affbb73b1aea58e6e
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-09 11:57:07 +0300

    add DataType

---
+++ Basics1/DataType/index.js
@@ -0,0 +1,21 @@
+// primitive Types.
+
+// Numbers.
+
+let age = 11, pi = 3.14;
+
+// BigInt 
+const bigInt = 54654654546545428734;
+
+// Strings.
+let name = 'alice';
+
+// booleans.
+let isStudent = true;
+
+// Null
+let length = null;
+
+// Undefined
+let grade;
+grade = '111';
