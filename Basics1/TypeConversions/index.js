5a0561636775f87b6d184aee3328c7dac5831a26
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-09 12:25:30 +0300

    add TypeConversions

---
+++ Basics1/TypeConversions/index.js
@@ -0,0 +1,23 @@
+let value = true;
+console.log(typeof value);
+
+// convert the $value to String
+value = String(value);
+console.log(typeof value);
+
+
+// convert String to a number
+let str = "5555";
+console.log(typeof str);
+let numb = Number(str);
+console.log(typeof numb);
+
+
+// Boolean
+console.log(Boolean(1)); //true
+console.log(Boolean(0)); //false
+console.log(Boolean("hello")); //true
+console.log(Boolean("")); //false
+
+console.log(Boolean('0')); //true
+console.log(Boolean('')); //true
