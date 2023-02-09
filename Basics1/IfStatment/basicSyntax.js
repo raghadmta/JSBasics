9471006bb94520fba5d35ad25a1997a3f8a80563
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-09 13:00:46 +0300

    add IfStatment

---
+++ Basics1/IfStatment/basicSyntax.js
@@ -0,0 +1,14 @@
+let year = prompt('In wich year JavaScript Relased');
+
+if (year == 1996) {
+    alert('Correct! ');
+}
+else {
+    year = prompt('In wich year JavaScript Relased');
+    if (year == 1996) {
+        alert('Correct! ');
+    }
+    else {
+        alert('wronng');
+    }
+}
---
+++ Basics1/IfStatment/index.js
@@ -0,0 +1,12 @@
+let day = prompt('Enter Day');
+
+
+if (day == 0) alert('Sunday');
+else if (day == 1) alert('Monday');
+else if (day == 2) alert('Tuesday');
+else if (day == 3) alert('Wednesday');
+else if (day == 4) alert('Thursday');
+else if (day == 5) alert('friday');
+else if (day == 6) alert('Saturday');
+
+else alert('not a day in the week');
