a64c707768ad8a7e3eea6ae6150936925b966c78
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-09 12:42:08 +0300

    add ArithmeticOperation

---
+++ Basics1/ArithmeticOperation/index.js
@@ -0,0 +1,26 @@
+console.log(8 + 2);
+console.log(8 - 2);
+console.log(8 * 2);
+console.log(8 / 2);
+
+// Reminder
+console.log(5 % 2);
+
+// Increcment & decrecment.
+let x = 7;
+console.log(++x);
+console.log(x);
+console.log(x++);
+console.log(x);
+console.log(--x);
+console.log(x);
+console.log(x--);
+console.log(x);
+
+
+// Contcatenation
+console.log('hi there' + 'how you doin?');
+console.log('1' + 3);
+console.log(1 + '3');
+
+console.log(1 + 2 + '33');
