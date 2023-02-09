367184e839a52192c7b75ed6518225bc5aa6cc8b
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-09 13:16:40 +0300

    add Ternary

---
+++ Basics1/Ternary/index.js
@@ -0,0 +1,6 @@
+let age = prompt("how old are you ?");
+
+// condtion ? value if true : value if false;
+// age >= 18 ? alert('you can drive !') : alert('you are not allowed to drive');
+let reslut = age >= 18 ? 'you can drive !' : 'you are not allowed to drive X';
+alert(reslut);
