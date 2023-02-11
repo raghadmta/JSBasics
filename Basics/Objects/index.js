5d9d9cc0c511ae532f0385c1db3af8d30c40d8b0
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-11 23:04:12 +0300

    added object functions

--- Basics/Objects/index.js
+++ Basics/Objects/index.js
@@ -1,6 +1,19 @@
-function Person(name,age,gender){
-    this.name= name;
-    this.age= age;
-    this.gender= gender;
-
+function Person(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.hello = function () {
+        console.log('hello there persons ! ');
+    };
 }
+
+// object info
+let person1 = new Person('raghad', 24, 'female'); // write person1 in the console
+
+
+// arrays of objects
+let persona = [
+    new Person('raghad', 25, 'female'),
+    new Person('raghad', 26, 'female'),
+    new Person('raghad', 27, 'female')
+]
