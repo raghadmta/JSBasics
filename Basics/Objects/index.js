d9bdf94be0d4ab4b8a1bac525bb3b078e9217c4d
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-12 19:17:48 +0300

    added Calue and refrence

--- Basics/Objects/index.js
+++ Basics/Objects/index.js
@@ -1,4 +1,4 @@
-// Premitve Types  String - Numbers - Boolean - undefind - null
+// Premitve Types  String - Numbers - Boolean - undefind - null 
 // Refrence Types  object - Function - Array
 
 let x = 23;
@@ -8,3 +8,11 @@ x = 20;
 let z = { name: 'Abdulrhaman' };
 let e = z;
 z.name = 'Ali';
+
+
+function increase(number){
+    number.value ++;
+}
+
+let number = {value: 1};
+increase(number);
