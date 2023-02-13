ba2f93c9634a3e7bd8820df03a6c2d5714b3b1a2
Raghad Almutlaq <Raghad.aziz.m@gmail.com>
2023-02-13 13:28:27 +0300

    added static in classes

---
+++ Basics/Classes/ClassesBasic.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    hello() {
+        console.log('HEllo world !! ');
+    }
+}
+
+const person = new Person('Raghad');
--- Basics/Classes/index.js
+++ Basics/Classes/index.js
@@ -5,6 +5,26 @@ class Person {
     hello() {
         console.log('HEllo world !! ');
     }
+    static staticMethodExample() {
+        console.log('Hello from static Methoed'); // run Person.staticMethodExample(); with the class name
+
+    }
 }
 
 const person = new Person('Raghad');
+
+
+class MyMath {
+    static sum(...args) {
+        let total = 0;
+        for (let num of args) total += num;
+        return total;
+
+    }
+    static multiply(...args) {
+        let total = 1;
+        for (let num of args) total *= num;
+        return total;
+
+    }
+}
